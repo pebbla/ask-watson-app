0dcb999384e0ae5d0acdb356c0ee3c9e3d7dd1dd
arock <arock1998@naver.com>
2022-08-18 01:25:05 +0900

    feat: 소셜 로그인 설정

---
+++ lib/auth/sign_up_view.dart
@@ -0,0 +1,53 @@
+import 'package:ask_watson_app/auth/sign_up_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<SignUpViewModel>(
+      create: (context) => SignUpViewModel(),
+      child: Scaffold(
+          appBar: AppBar(title: const Text('로그인')),
+          body: SignUpView(),
+        )
+    );
+  }
+}
+
+
+class SignUpView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: Center(child: _signUpKakaoBtn(context)),
+    );
+  }
+
+  /**
+   * 카카오 로그인하기 버튼
+   */
+  Widget _signUpKakaoBtn(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Provider.of<SignUpViewModel>(context, listen: false).tabSignUpKakaoBtn(context),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(255, 234, 0, 1),
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        child: Row(
+          children: [
+            Image.asset('assets/auth/kakao_logo_icon.png'),
+            const Padding(padding: EdgeInsets.all(4)),
+            const Text(
+              '카카오 로그인하기',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
