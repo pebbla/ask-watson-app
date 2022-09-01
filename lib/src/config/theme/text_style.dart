d7de0fa1efbf3a480b62cb29f31bff41fef2b7cc
arock <arock1998@naver.com>
2022-09-01 23:08:26 +0900

    feat: main app 개요

--- lib/src/config/theme/text_style.dart
+++ lib/src/config/theme/text_style.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
 class MyTextStyle {
+
+  static const TextStyle black18w600 = TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: MyColor.black);
+
+  static const TextStyle black14w500 = TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: MyColor.black);
+
+  static const TextStyle black12w500 = TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: MyColor.black);
+
+
+
   
 }
