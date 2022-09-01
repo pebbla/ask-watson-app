e6d0ba734a18cf72432ea41afc6b6bd3b8a26afa
arock <arock1998@naver.com>
2022-09-01 19:16:26 +0900

    chore: 필요없는 코드, import 삭제

--- lib/src/presentation/main/main_screen.dart
+++ lib/src/presentation/main/main_screen.dart
@@ -1,9 +1,3 @@
-import 'package:ask_watson_app/src/data/repository/faq_repository_impl.dart';
-import 'package:ask_watson_app/src/data/repository/location_repository_impl.dart';
-import 'package:ask_watson_app/src/data/repository/notice_repository_impl.dart';
-import 'package:ask_watson_app/src/domain/use_case/faq_use_case.dart';
-import 'package:ask_watson_app/src/domain/use_case/location_user_case.dart';
-import 'package:ask_watson_app/src/domain/use_case/notice_use_case.dart';
 import 'package:ask_watson_app/src/presentation/main/main_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -39,12 +33,6 @@ class MainView extends StatelessWidget {
           padding: const EdgeInsets.all(24),
           child: Column(
             children: [
-
-              TextButton(onPressed: (){
-                 var usecase =  LocationUseCase(LocationRepositoryImpl());
-                 usecase.getLocationList();
-              }, child: Text('테스트를 위해서 눌러주세요')),
-
               _buildSearchBar(context),
               _buildBody(context),
             ],
