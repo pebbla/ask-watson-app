ec3450d43f16a498963161f2ce6b88084e9f5c10
arock <arock1998@naver.com>
2022-09-03 22:25:09 +0900

    feat: star rating widget

--- lib/src/config/theme/text_style.dart
+++ lib/src/config/theme/text_style.dart
@@ -4,11 +4,22 @@ import 'colors.dart';
 
 class MyTextStyle {
 
+  // 검정색
   static const TextStyle black18w600 = TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: MyColor.black);
+  static const TextStyle black16w600 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: MyColor.black);
 
   static const TextStyle black14w500 = TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: MyColor.black);
 
   static const TextStyle black12w500 = TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: MyColor.black);
+
+
+  // 회색
+  static const TextStyle grey14w500 = TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: MyColor.grey);
+
+  
+  // 밝은 회색
+  static const TextStyle lightGrey14w500 = TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: MyColor.lightGrey);
+
 
 
 
--- lib/src/presentation/main/main_screen.dart
+++ lib/src/presentation/main/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:ask_watson_app/src/config/theme/text_style.dart';
 import 'package:ask_watson_app/src/data/model/cafe.dart';
 import 'package:ask_watson_app/src/data/model/theme.dart' as m;
 import 'package:ask_watson_app/src/presentation/main/main_view_model.dart';
+import 'package:ask_watson_app/src/presentation/widget/star_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -31,17 +32,22 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              children: [
-                _buildSearchBar(context),
-                _buildCategoryList(context),
-                _buildCafeList(context),
-                _buildThemeList(context),
-              ],
+      body: GestureDetector(
+        onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                children: [
+                  _buildSearchBar(context),
+                  _buildCategoryList(context),
+                  _buildCafeList(context),
+                  _buildThemeList(context),
+                ],
+              ),
             ),
           ),
         ),
@@ -49,65 +55,71 @@ class MainView extends StatelessWidget {
     );
   }
 
-  Widget _buildSearchBar(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: 90,
-            height: 120,
-            child: Container(),),
 
-          Flexible(
-            child: Container(
-              height: 56,
-              child: TextField(
-                onChanged: (text) => viewModelWatch.onChanged(),
-                controller: viewModel.controller,
-                focusNode: viewModel.focusNode,
-                decoration: InputDecoration(
-                    isDense: true,
-                    prefix: Icon(
-                      Icons.search,
-                      color: MyColor.grey,
-                    ),
-                    hintText: '검색어를 입력하세요',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide:BorderSide(color: MyColor.grey)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide:BorderSide(color: MyColor.grey))),
-              ),
+  // 검색창 
+  Widget _buildSearchBar(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          width: 90,
+          height: 120,
+          child: Container(
+            //TODO : 사진 넣기
+          ),),
+        Flexible(
+          child: Container(
+            height: 56,
+            child: TextField(
+              onChanged: (text) => viewModelWatch.onChanged(),
+              controller: viewModel.controller,
+              focusNode: viewModel.focusNode,
+              cursorColor: MyColor.lightGrey,
+              decoration: InputDecoration(
+                  isDense: true,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: MyColor.lightGrey,
+                  ),
+                  hintText: '검색어를 입력하세요',
+                  hintStyle: MyTextStyle.lightGrey14w500,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide:BorderSide(color: MyColor.lightGrey)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide:BorderSide(color: MyColor.lightGrey)),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide:BorderSide(color: MyColor.lightGrey))),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  // 카테고리
+
+  // 카테고리 리스트
   Widget _buildCategoryList(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(12),
+      padding: EdgeInsets.only(bottom: 18, top: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('카테고리', style: MyTextStyle.black18w600),
-          Padding(padding: EdgeInsets.all(8)),
+          Padding(padding: EdgeInsets.all(4)),
           Row(
             children: [
               _categoryItem('', '스릴러'),
               Padding(padding: EdgeInsets.all(8)),
-              _categoryItem('', '스릴러'),
+              _categoryItem('', '공포'),
               Padding(padding: EdgeInsets.all(8)),
               _categoryItem('', '스릴러'),
               Padding(padding: EdgeInsets.all(8)),
               _categoryItem('', '스릴러'),
             ],
-          )
+          ),
         ],
       ),
     );
@@ -120,11 +132,13 @@ class MainView extends StatelessWidget {
         children: [
           Container(
             padding: EdgeInsets.all(8),
+            width: 55,
+            height: 55,
             decoration: BoxDecoration(
               border: Border.all(),
               borderRadius: BorderRadius.all(Radius.circular(50))
             ),
-            child: Image.asset('assets/auth/kakao_logo_icon.png'),
+            // child: Image.asset('assets/auth/kakao_logo_icon.png'),
           ),
           Padding(padding: EdgeInsets.all(4)),
           Text(categoryName, style: MyTextStyle.black12w500,)
@@ -140,7 +154,7 @@ class MainView extends StatelessWidget {
     Cafe cafe = Cafe();
     return Container(
       width: double.maxFinite,
-      padding: EdgeInsets.all(12),
+      padding: EdgeInsets.only(top: 12, bottom: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -157,41 +171,58 @@ class MainView extends StatelessWidget {
               )
             ],
           ),
-          Padding(padding: EdgeInsets.all(8)),
-          Row(children: [
-            _cafeItem(cafe),
-            Padding(padding: EdgeInsets.all(4)),
-            _cafeItem(cafe),
-
-          ],)
+          Row(
+            children: [
+              _cafeItem(cafe),
+              Padding(padding: EdgeInsets.all(4)),
+              _cafeItem(cafe),
+            ],
+          ),
         ],
       ),
     );
   }
 
-
-
   // 카페 아이템
   Widget _cafeItem(Cafe cafe) {
     return Flexible(
       child: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(24))),
+        padding: EdgeInsets.only(bottom: 8),
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(12))),
         width: double.maxFinite,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
               width: double.maxFinite,
-              height: 140,
+              height: 160,
               //TODO : 사진 넣기
-              child: Container(),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: MyColor.lightlightGrey,
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+              ),
             ),
-            Text(cafe.cafeName ?? ''),
-            // TODO : star widget
-            Row(
-              children: [
-                // star widget,
-                Text('${cafe.rating}')
-              ],
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${cafe.name ?? '키이스케이프 강남점' }', style: MyTextStyle.black16w600,),
+                  // TODO : star widget
+                  Padding(padding: EdgeInsets.all(2)),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // star widget,
+                      StarWidget(rating: cafe.rating ?? 4.0),
+                      Padding(padding: EdgeInsets.all(2)),
+                      Text('${cafe.rating ?? 0.0}', style: MyTextStyle.grey14w500)
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -205,7 +236,7 @@ class MainView extends StatelessWidget {
     m.Theme theme = m.Theme();
     return Container(
       width: double.maxFinite,
-      padding: EdgeInsets.all(12),
+      padding: EdgeInsets.only(top: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -222,7 +253,6 @@ class MainView extends StatelessWidget {
               )
             ],
           ),
-          Padding(padding: EdgeInsets.all(8)),
           Row(children: [
             _themeItem(theme),
             Padding(padding: EdgeInsets.all(4)),
@@ -240,23 +270,42 @@ class MainView extends StatelessWidget {
   Widget _themeItem(m.Theme theme) {
     return Flexible(
       child: Container(
-        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(24))),
+        padding: EdgeInsets.only(bottom: 8),
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(12))),
         width: double.maxFinite,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
               width: double.maxFinite,
-              height: 140,
+              height: 160,
               //TODO : 사진 넣기
-              child: Container(),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: MyColor.lightlightGrey,
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+              ),
             ),
-            Text(theme.themeName ?? ''),
-            // TODO : star widget
-            Row(
-              children: [
-                // star widget,
-                Text('${theme.rating}')
-              ],
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${theme.name ?? '키이스케이프 강남점' }', style: MyTextStyle.black16w600,),
+                  // TODO : star widget
+                  Padding(padding: EdgeInsets.all(2)),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // star widget,
+                      StarWidget(rating: theme.rating ?? 0.0),
+                      Padding(padding: EdgeInsets.all(2)),
+                      Text('${theme.rating ?? 0.0}', style: MyTextStyle.grey14w500)
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
