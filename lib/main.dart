bdbfdb6942fbf6feee981f61b7737b782ff04d68
arock <arock1998@naver.com>
2022-09-08 19:10:31 +0900

    feat: tab bar 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-
-import 'package:ask_watson_app/src/presentation/mypage/mypage_screen.dart';
+import 'package:ask_watson_app/src/presentation/provider/app_set_up.dart';
+import 'package:ask_watson_app/src/presentation/tab/tab_bar_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-
-
+import 'package:provider/provider.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: '8c7b05953960c527e38fabb76c758817');
@@ -11,12 +10,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ask Watson',
-      home: MypageScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => SetUp()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        home: MyStatefulWidget(),
+      ),
+    );
   }
 }
