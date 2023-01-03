2bd3870a95cba0e3712f1b355897cbb9b7bbd40c
arock <arock1998@naver.com>
2023-01-03 13:52:18 +0900

    feat: 카페 검색결과 UI

--- lib/src/presentation/search/search_screen.dart
+++ lib/src/presentation/search/search_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:ask_watson_app/src/config/theme/colors.dart';
 import 'package:ask_watson_app/src/config/theme/text_style.dart';
+import 'package:ask_watson_app/src/data/model/cafe.dart';
 import 'package:ask_watson_app/src/presentation/search/search_view_model.dart';
+import 'package:ask_watson_app/src/presentation/widget/star_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -29,6 +33,8 @@ class SearchScreen extends StatelessWidget {
             child: Column(
               children: [
                 _searchTextField(viewModel),
+                _cafeList(viewModel.cafeList),
+                Divider(thickness: 10,)
               ],
             ),
           ),
@@ -63,6 +69,80 @@ class SearchScreen extends StatelessWidget {
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(8)),
                 borderSide: BorderSide(color: MyColor.grey))),
+      ),
+    );
+  }
+
+  // 카페 리스트
+  Widget _cafeList(List<Cafe> cafeList) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 8),
+      child: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("카페 검색결과 ${cafeList.length}",
+                  style: MyTextStyle.black18w600),
+              cafeList.length > 0 ? Text("더보기 >", style: MyTextStyle.grey14w500) : Container(),
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(4)),
+          Divider(thickness: 1, color: MyColor.grey),
+
+
+          // // 카페 리스트 보기
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: min(3, cafeList.length),
+            itemBuilder: (BuildContext context, index) {
+            return _cafeitem(cafeList[index]);
+          })
+        ],
+      ),
+    );
+  }
+
+  // 카페 아이템
+  Widget _cafeitem(Cafe cafe) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      height: 88,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 72,
+            height: 72,
+            decoration:
+                BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+            // TODO 여기에 사진 넣기
+            child: Container(),
+          ),
+          Padding(padding: EdgeInsets.all(8)),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("${cafe.name ?? '포인트나인 강남점'}",
+                  style: MyTextStyle.black16w500),
+              Padding(padding: EdgeInsets.all(1)),
+              StarWidget(
+                  rating: double.parse((cafe.name ?? 0).toString()),
+                  color: MyColor.black),
+              Padding(padding: EdgeInsets.all(2)),
+              // TODO 현재 위치로부터 떨어져 있는 거리
+              Row(
+                children: [
+                  Icon(Icons.pin_drop_outlined, size: 18),
+                  Padding(padding: EdgeInsets.all(2)),
+                  Text("거리 정보 없음", style: MyTextStyle.grey14w500)
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/src/presentation/search/search_view_model.dart
+++ lib/src/presentation/search/search_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:ask_watson_app/src/data/model/cafe.dart';
+import 'package:ask_watson_app/src/data/model/theme.dart' as m;
 import 'package:ask_watson_app/src/data/repository/cafe_repository_impl.dart';
 import 'package:ask_watson_app/src/data/repository/theme_repository_impl.dart';
 import 'package:ask_watson_app/src/domain/use_case/cafe_use_case.dart';
@@ -9,12 +11,17 @@ class SearchViewModel extends ChangeNotifier {
   CafeUseCase _cafeUseCase = CafeUseCase(CafeRepositoryImpl());
   ThemeUseCase _themeUseCase = ThemeUseCase(ThemeRepositoryImpl());
 
+  List<Cafe> _cafeList = [Cafe(), Cafe(),Cafe(),Cafe(),Cafe(),Cafe(),];
+  List<Cafe> get cafeList => _cafeList;
+
+  List<m.Theme> _themeList = [];
+  List<m.Theme> get themeList => _themeList;
+
   void onChanged(String text){
     print("on Changed 함수가 불렸습니다.");
     // TODO : debounce 적용
     _cafeUseCase.getCafeListBySearch(text);
     _themeUseCase.getThemeListBySearch(text);
-
   }
   
 }
